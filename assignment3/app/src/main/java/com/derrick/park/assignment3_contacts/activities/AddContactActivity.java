4efa4bb32a875e9ef4b181db3deaa46a563efd96
Kazuya <shiozaki5621@gmail.com>
2019-01-18 00:06:26 -0800

    Add validator to assignment 3

--- assignment3/app/src/main/java/com/derrick/park/assignment3_contacts/activities/AddContactActivity.java
+++ assignment3/app/src/main/java/com/derrick/park/assignment3_contacts/activities/AddContactActivity.java
@@ -3,7 +3,10 @@ package com.derrick.park.assignment3_contacts.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.derrick.park.assignment3_contacts.R;
@@ -12,6 +15,7 @@ public class AddContactActivity extends AppCompatActivity {
 
     private TextView mAddNameText;
     private TextView mAddPhoneText;
+    private Button mSubmitButton;
 
     final static public String ADD_USER_NAME = "userName";
     final static public String ADD_USER_PHONE = "userPhone";
@@ -24,8 +28,50 @@ public class AddContactActivity extends AppCompatActivity {
         // set TextView
         mAddNameText = findViewById(R.id.addName);
         mAddPhoneText = findViewById(R.id.addPhone);
-    }
+        mSubmitButton = findViewById(R.id.addButton);
 
+        mAddNameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // nothing to do
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // nothing to do
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (isValid()) {
+                    mSubmitButton.setEnabled(true);
+                } else {
+                    mSubmitButton.setEnabled(false);
+                }
+            }
+        });
+
+        mAddPhoneText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //nothing to do
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //nothing to do
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (isValid()) {
+                    mSubmitButton.setEnabled(true);
+                } else {
+                    mSubmitButton.setEnabled(false);
+                }
+            }
+        });
+    }
 
     public void submitContact(View view) {
         // create Intent
@@ -41,4 +87,26 @@ public class AddContactActivity extends AppCompatActivity {
         // finish this activity
         finish();
     }
+
+
+    private boolean isValid() {
+        boolean isValid = true;
+
+        if(!mAddNameText.getText().toString().isEmpty() && mAddNameText.getText().toString().matches(".+ .+")) {
+            //name is valid
+        } else {
+            //name is invalid
+            isValid = false;
+        }
+
+        if(mAddPhoneText.getText().toString().length() == 10) {
+            // phone is valid
+        } else {
+            // phone is invalid
+            isValid =false;
+        }
+
+        return isValid;
+    }
+
 }
